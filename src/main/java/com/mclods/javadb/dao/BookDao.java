4aaf648f73fc834217750f4450e7805545164939
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-02-23 01:20:32 +0530

    Added create book dao

--- src/main/java/com/mclods/javadb/dao/BookDao.java
+++ src/main/java/com/mclods/javadb/dao/BookDao.java
@@ -1,4 +1,7 @@
 package com.mclods.javadb.dao;
 
+import com.mclods.javadb.domain.Book;
+
 public interface BookDao {
+    void create(Book book);
 }
--- src/main/java/com/mclods/javadb/dao/impl/BookDaoImpl.java
+++ src/main/java/com/mclods/javadb/dao/impl/BookDaoImpl.java
@@ -1,6 +1,7 @@
 package com.mclods.javadb.dao.impl;
 
 import com.mclods.javadb.dao.BookDao;
+import com.mclods.javadb.domain.Book;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class BookDaoImpl implements BookDao {
@@ -8,5 +9,10 @@ public class BookDaoImpl implements BookDao {
 
     public BookDaoImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void create(Book book) {
+        jdbcTemplate.update("INSERT INTO book(title, author_id, isbn) VALUES(?, ?, ?)", book.getTitle(), book.getAuthorId(), book.getIsbn());
     }
 }
